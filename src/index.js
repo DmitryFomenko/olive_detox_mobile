48217a1e7add5e0eb95d5ba20290a6d8b6200434
Dmytro <voronok16@gmail.com>
2019-11-26 11:59:07 +0200

    mobile v0.2

--- src/index.js
+++ src/index.js
@@ -23,57 +23,6 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
-  $(function () {
-    let w = $(window);
-
-    w.on('scroll', function () {
-      let top = $(this).scrollTop();
-      parallax(top);
-    });
-  });
-
-  function parallax(top) {
-    if(top > 300) {
-      $('.header-info__leaf_up').css(
-      'transform', 'translateY(' + ((top / -10) + 32) + 'px)'
-      );
-      $('.header-info__leaf_down').css(
-        'transform', 'translateY(' + ((top / -10) + 32) + 'px)'
-      );
-    }
-
-    if(top > 1900) {
-        $('.detox__leaf-left').css(
-      'transform', 'translateY(' + ((top / -10) + 190) + 'px)'
-      );
-      $('.detox__leaf-right').css(
-        'transform', 'translateY(' + ((top / -7) + 271) + 'px)'
-      );
-    }
-
-    if(top > 2300) {
-      $('.detox__leaf-down').css(
-      'transform', 'translateY(' + ((top / -10) + 238) + 'px)'
-      )
-    }
-
-    if(top > 4800) {
-      $('.leaf_find_left').css(
-      'transform', 'translateY(' + ((top / -10) + 480) + 'px)'
-      );
-      $('.leaf_find_right').css(
-      'transform', 'translateY(' + ((top / -10) + 480) + 'px)'
-      );
-
-      $('.leaf_find_left_down').css(
-        'transform', 'translateY(' + ((top / -10) + 480) + 'px)'
-      );
-    }
-  };
-
-})
-
 $(document).ready(function(){
   $(".checkbox1").click(function(){
       $(".nav").toggleClass("show");
